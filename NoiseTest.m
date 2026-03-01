ad882d9ad982cd411730ea88a80f6d700b44171b
nickcastalano <nickcast@vt.edu>
2026-03-01 13:00:41 -0500

    Delete NoiseTest.m

--- NoiseTest.m
+++
@@ -1,8 +0,0 @@
-classdef NoiseTest < matlab.unittest.TestCase
-    methods (Test)
-        function noiseFunc(testCase)
-            noise = noiseFunc();
-            testCase.verifyLessThan(noise, 75)
-        end
-    end
-end
