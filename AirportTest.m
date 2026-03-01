3f1c1a9c47e4e524cad02be600925c59ad6767fe
nickcastalano <nickcast@vt.edu>
2026-03-01 12:57:17 -0500

    Delete AirportTest.m

--- AirportTest.m
+++
@@ -1,8 +0,0 @@
-classdef AirportTest < matlab.unittest.TestCase
-    methods (Test)
-        function airportCompatibiltyFunc(testCase)
-            airports = airportCompatibiltyFunc();
-            testCase.verifyLessThan(airports, 40)
-        end
-    end
-end
