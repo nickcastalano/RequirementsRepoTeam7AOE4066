30d61b0cc97df9c35c39a1aa71a9617e6a52366a
nickcastalano <nickcast@vt.edu>
2026-03-01 12:54:40 -0500

    Delete ADGTest.m

--- ADGTest.m
+++
@@ -1,9 +0,0 @@
-classdef ADGTest < matlab.unittest.TestCase
-    methods (Test)
-        function range_func(testCase)
-            [wingSpan, tailHeight] = gateCompatibiltyFunc();
-            testCase.verifyLessThan(wingSpan, 214)
-            testCase.verifyLessThan(tailHeight, 66)
-        end
-    end
-end
